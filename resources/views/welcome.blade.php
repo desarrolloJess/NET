19119c359ad5d3f3d7347555ec3e8635cd9bd20c
desarrolloJess <jg.delgadochavez@gmail.com>
2023-02-27 22:46:11 -0600

    primer commit se sube archivo

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NETFEX</title>
+    <link href="{{ asset('css/estilos.css') }}" rel="stylesheet">   
+
+
+</head>
+
+<body>
+    <section id="hero">
+        <img src="img/logoEmpresaNETMEX.png" alt="NETMEX" id="heroImg">
+    <!-- #region BARRA NAVEGACION -->
+        <nav>
+            <ul>
+                <li><a href="default.html">Home</a></li>
+                <li><a href="news.asp">About Us</a></li>
+                <li><a href="contact.asp">Movies</a></li>
+                <li><a href="about.asp">Contac</a></li>
+                <li><a href="about.asp">Sign in</a></li>
+            </ul>
+        </nav>
+</section>
+    <!-- #endregion -->
+        
+    <section id="nav1">
+
+    </section>
+    
+</body>
+</html>
